7042f7a85072fb904d3076a240c3c0ec697f0aa8
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-05 10:16:33 -0500

    Added a elements module

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import {Books} from './modules/books.js';  // Books constructor.
 import {Methods} from './modules/methods.js';  // Books methods. 
-
+import {mainContainer, titleInput, authorInput, submitButton, form} from './modules/elements.js';
---
+++ modules/elements.js
@@ -0,0 +1,7 @@
+const mainContainer = document.querySelector('#books-container');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const submitButton = document.querySelector('#book-submit');
+const form = document.querySelector('#form-book');
+
+export {mainContainer, titleInput, authorInput, submitButton, form};
