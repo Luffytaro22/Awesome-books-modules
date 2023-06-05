2fe5793db5edbc89730218d80a23fd679d2412ef
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-05 10:45:58 -0500

    Added more elements

--- modules/elements.js
+++ modules/elements.js
@@ -4,4 +4,15 @@ const authorInput = document.querySelector('#author');
 const submitButton = document.querySelector('#book-submit');
 const form = document.querySelector('#form-book');
 
-export {mainContainer, titleInput, authorInput, submitButton, form};
+const pDate = document.querySelector('#date');
+const list = document.querySelector('.nav-sections:nth-child(1)');
+const addNew = document.querySelector('.nav-sections:nth-child(2)');
+const contact = document.querySelector('.nav-sections:nth-child(3)');
+const booksSection = document.querySelector('#books-section');
+const formSection = document.querySelector('#addBook-section');
+const contactSection = document.querySelector('#contact');
+const iconList = document.querySelector('#icon-list');
+const iconBook = document.querySelector('#icon-book');
+const iconContact = document.querySelector('#icon-contact');
+
+export {mainContainer, titleInput, authorInput, submitButton, form, pDate, list, addNew, contact, booksSection, formSection, contactSection, iconList, iconBook, iconContact};
