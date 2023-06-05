d8dd4233a12fb0a3e0de9117b2eb764ac8c8fc98
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-05 10:46:15 -0500

    Added listeners to the sections

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import {Methods} from './modules/methods.js';  // Books methods. 
-import {submitButton} from './modules/elements.js';
+import {submitButton, list, iconList, addNew, iconBook, contact, iconContact} from './modules/elements.js';
 import {addBook} from './modules/addBook.js';
+import { showBooks, showContact, showForm } from './modules/sections.js';
 
 const myBooks = new Methods();
 myBooks.displayBooks();
 
 submitButton.addEventListener('click', addBook);
+
+list.addEventListener('click', showBooks);
+iconList.addEventListener('click', showBooks);
+
+addNew.addEventListener('click', showForm);
+iconBook.addEventListener('click', showForm);
+
+contact.addEventListener('click', showContact);
+iconContact.addEventListener('click', showContact);
