62d11bea806fb32c8646cc9d543c1420072bf8bd
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-05 10:14:10 -0500

    Extend the books class from the methods class

--- modules/books.js
+++ modules/books.js
@@ -1,5 +1,8 @@
-export class Books {
+import { Methods } from "./methods";  // Books methods
+
+export class Books extends Methods {
   constructor(title, author) {
+    super();
     this.title = title;
     this.author = author;
   }
